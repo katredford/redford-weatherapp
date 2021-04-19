3c3452d528118be635f38ee29270e58ce9698a75
katredford <katharine.redford@gmail.com>
2021-04-18 20:05:28 -0500

    fix local storage, make cities click and display on page

--- Weather_dashboard/Assets/script.js
+++ Weather_dashboard/Assets/script.js
@@ -1,10 +1,97 @@
+// var recentCity = document.createElement("p");
 
-function citySearch(){
-  var city = document.getElementById("searchBox").value
+// var userCity = document.getElementById("searchBox")
+
+// if(localStorage.getItem("saved-cities")){
+//   savedCities = JSON.parse(localStorage.getItem("saved-cities"))
+// }
+
+// var saveCities = document.getElementById("saveBtn")
+// saveCities.addEventListener("click", saveCity);
+// console.log(localStorage)
+// var displayCities = function() {
+  
+  
+// }
+
+
+
+
+
+// function saveCity() {
+//   var cityPlace = document.getElementById("city")
+//   savedCities.push(userCity)
+//   localStorage.setItem("saved-cities", JSON.stringify(savedCities));
+  
+//   var cities = localStorage.getItem("saved-cities")
+// };
+
+
+
+
+
+
+var searchedCity = document.getElementById("searchBox")
+const saveButton = document.getElementById("saveBtn")
+const recentCity = document.getElementById("searchPlace")
+
+var savedCities = [];
+saveButton.onclick = function() {
+  const citySearched = searchedCity.value;
+  
+  if(citySearched){
+    savedCities.push(citySearched)
+    localStorage.setItem("city", JSON.stringify(savedCities));
+
+    displayHistory()
+    
+  }
+
+  var city = localStorage.getItem("city")
+  citySearch(document.getElementById("searchBox").value)
+
+}
+
+  function displayHistory(){
+    // var city = localStorage.getItem("city")
+    if(localStorage.getItem("city")){
+
+      savedCities = JSON.parse(localStorage.getItem("city"))
+
+    }
+
+    console.log('saved cites array!',savedCities)
+    const searchDisplay = document.getElementById("searchPlace")
+    searchDisplay.innerHTML = ""
+    for (let i = 0; i < savedCities.length; i++) {
+
+      var recentCity = document.createElement("li")
+      recentCity.setAttribute("class", "history-city")
+
+      recentCity.innerHTML = savedCities[i]
+
+      searchDisplay.appendChild(recentCity)
+      console.log(recentCity)
+    }
+
+  }
+displayHistory()
+
+// var pastCity = document.getElementsByClassName("history-city")
+// pastCity.onclick = function () {
+//   console.log("get clicked!")
+// }
+$(document).on('click', '.history-city', function() {
+    console.log('we got clicked!!')
+    citySearch($(this).text())
+})
+
+
+function citySearch(city){
+  var city = city
   const cityDisplay = document.getElementById("cityPlace")
 
   cityDisplay.textContent = city
-   console.log(city)
    
   fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=cea924180544dde5b612be105dafb515&units=imperial")
   
@@ -12,15 +99,7 @@ function citySearch(){
       return response.json();
     })
     .then(function(data) {
-      // console.log(data.coord);
-  
-      // var locationIcon = document.querySelector('.weather-icon');
-      // var {icon} = data.weather[0];
-      // locationIcon.innerHTML = <img src="icons/${icon}.png">;
-      // var city = document.getElementById("city")
-      // city.textContent = data.name
-  
-
+ 
       const latitude = data.coord.lat
       const longitude = data.coord.lon
   
@@ -32,14 +111,13 @@ function citySearch(){
       }) 
       .then(function(data) {
         const dateTime = data.current.dt
-        console.log(dayjs.unix(dateTime).format("MMMM D, YYYY"))
-        console.log(data)
+    
 
         var date =document.getElementById("datePlace")
         date.innerHTML = dayjs.unix(dateTime).format("MMMM D, YYYY")
 
         var temp = document.getElementById("tempNumbers")
-        temp.textContent = data.current.temp
+        temp.textContent = "Temp: " +  data.current.temp
     
         var humid = document.getElementById("humNumbers")
         humid.textContent = data.current.humidity
@@ -48,10 +126,19 @@ function citySearch(){
         wind.textContent = data.current.wind_speed
     
         var uv = document.getElementById("uvNumbers")
+        if(data.current.uvi <= 4){
+          uv.setAttribute("class", "favorable")
+        } else if(data.current.uvi > 4 && data.current.uvi <= 7){
+          uv.setAttribute("class", "moderate")
+        }else{
+          uv.setAttribute("class", "severe")
+        }
         uv.textContent = data.current.uvi
 
         // console.log(data.daily[0])
         var forcastData = data.daily
+        const displayBox = document.getElementById("dailyPlace")
+        displayBox.innerHTML = ""
         for (let i = 0; i < 5; i++) {
         var dailyDisplay = forcastData[i];
         const dateDaily = dailyDisplay.dt
@@ -60,7 +147,7 @@ function citySearch(){
         
         const tempDaily = dailyDisplay.temp.day
         const dailyTemp = document.createElement("p")
-        dailyTemp.textContent = tempDaily
+        dailyTemp.textContent = "Temp: " + tempDaily
         
         const humidDaily = dailyDisplay.humidity
         const dailyHumid = document.createElement("p")
@@ -72,7 +159,7 @@ function citySearch(){
           
 
         const dailyDate = document.createElement("div")
-        const displayBox = document.getElementById("dailyPlace")
+       
         dailyDate.setAttribute("class", "dayBox col")
         dailyDate.innerHTML = dateDisplay 
      
